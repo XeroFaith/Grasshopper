35a005ef707d37d2955b543644d7b5fc477217d9
lightbringer <xero.faith@hush.ai>
2015-12-18 00:30:14 -0600

    Removed random search list Replaced random search list with method that hits image carousel Added method to get dailies (minus the trivia)

--- grasshopper.py
+++ grasshopper.py
@@ -18,7 +18,7 @@ import sys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import datetime
 
 # uname = sys.argv[1]
 # pword = sys.argv[2]
@@ -56,7 +56,8 @@ def sign_in(browser, email, password):
 
 def runCarousel(browser):
 
-    browser.get('http://www.bing.com/')  # open bing
+    if (browser.current_url != "https://www.bing.com/"):
+        browser.get('https://www.bing.com/')  # open bing
 
     try:
         element = WebDriverWait(browser, 10).until(
@@ -101,6 +102,7 @@ def runCarousel(browser):
         image = item.find_element_by_xpath("./a[1]")
         href = image.get_attribute("href")
         hrefs.append(href)
+
 
 
     for link in hrefs:
@@ -148,30 +150,83 @@ def runDailies(browser):
         time.sleep(wait_time)  # pause time
 
 
+def runMobile(browser):
+
+
+
+    if (browser.current_url != "https://www.bing.com/"):
+        browser.get('https://www.bing.com/')  # open bing
+
+    try:
+        element = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "pop_tile"))
+        )
+    finally:
+        # dailyPage is the set of all of the gray box groups
+        first = browser.find_element_by_class_name("pop_tile")
+
+        firstLink = first.get_attribute("href")
+
+        browser.get(firstLink)
+
+    try:
+        element = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "card"))
+        )
+    finally:
+        # dailyPage is the set of all of the gray box groups
+        cards = browser.find_elements_by_class_name("card")
+
+    hrefs = []
+
+    for card in cards:
+        a_tag = card.find_element_by_tag_name("a")
+        href = a_tag.get_attribute("href")
+        hrefs.append(href)
+
+    for href in hrefs[:25]:
+        wait_time = random.randint(10, 50) / 10
+        browser.get(href)
+        time.sleep(wait_time)  # pause time
 ## --------------------------------------------------------------------------------------------------------- ##
 
 def main():
 
+    uname = sys.argv[1]
+    pword = sys.argv[2]
+
+    print("Account: ",uname,'\n',
+          "Begin: ",datetime.date.today(),'\n\n'
+          )
+
 
     ff = webdriver.Firefox()  # open Firefox
-
-    print('Begin Script')
-
-    uname = 'jondavid0117@outlook.com'
-    pword = 'P@$$word11'
 
     sign_in(ff, uname, pword)
 
     time.sleep(random.randint(20, 100) / 10)  # suspend script to allow for sign in process to complete
 
-    runDailies(ff)
+    runCarousel(ff)
 
-    #runCarousel(ff)
+    runDailies(ff)
 
     ff.quit()
 
-    input()
+    iOS = webdriver.FirefoxProfile()
+    iOS.set_preference("general.useragent.override",
+                           "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25")
 
+    iOS_FF = webdriver.Firefox(iOS)
+
+    sign_in(iOS_FF, uname, pword)
+
+    runMobile(iOS_FF)
+
+    iOS_FF.quit()
+
+    print("Account: ",uname,'\n',
+          "End: ",datetime.date.today(),'\n\n'
+          )
 ## --------------------------------------------------------------------------------------------------------- ##
 
 
